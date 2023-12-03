956c69105e3df63e6725a1e4648edbbdddd9339c
unknown <chris@chris.com>
2023-12-03 15:43:47 -0500

    two

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,48 @@
-const {src,dest, watch} = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const plumber = require("gulp-plumber")
+const plumber = require("gulp-plumber");
 
-function css(done){
+// Usa require para cargar gulp-webp
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
 
-    src('src/scss/**/*.scss') //identidicar el archivo sass
-    .pipe(plumber())
-    .pipe(sass()) //compilarlo
-    .pipe(dest("build/css")); //alamcenar en el disco duro
-
+function css(done) {
+    src('src/scss/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(dest("build/css"));
     done();
 }
 
-function dev(done){
-    watch('src/scss/**/*.scss', css);
+function imagenes(done){
+    const opciones = {
+        optimizationLevel: 3
+    }
 
+    src("src/img/**/*.{png,jpg}")
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest("build/img"))
+    done()   
+}
+
+function versionWebp(done) {
+    const opciones = {
+        quality: 50
+    };
+
+    src("src/img/**/*.{png,jpg}")
+        .pipe(webp(opciones))
+        .pipe(dest("build/img"));
+    done();
+}
+
+function dev(done) {
+    watch('src/scss/**/*.scss', css);
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(imagenes,versionWebp, dev);
